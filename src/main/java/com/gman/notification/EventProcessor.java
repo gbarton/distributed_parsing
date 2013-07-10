6a505cb33369a5df9fea795e28b13c667b12798a
gman <gman@gman-es.(none)>
2013-07-10 10:30:28 -0400

    tryin to get kafka messaging up and running

--- src/main/java/com/gman/notification/EventProcessor.java
+++ src/main/java/com/gman/notification/EventProcessor.java
@@ -1,5 +1,12 @@
 package com.gman.notification;
 
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
 /**
  * Base Class for notifications traveling through the system.
  * @author gman
@@ -27,13 +34,64 @@ public interface EventProcessor {
 	
 	public WorkUnit getNextWorkUnit();
 	
-	public static class WorkUnit {
+	/**
+	 * little interface to build to if you are going to send messages
+	 * over Kafka
+	 */
+	public interface KMesg<T> {
+		public String pack();
+		public T unpack(String packed);
+	}
+	
+	public static class WorkUnit implements KMesg<WorkUnit> {
 		public String work;
 		public String pathIn = null;
 		public String pathOut = null;
 		
 		public String toString() {
 			return "pathIn: " + pathIn + " pathOut: " + pathOut;
+		}
+
+		@Override
+		public String pack() {
+			ObjectMapper mapper = new ObjectMapper();
+			try {
+				return mapper.writeValueAsString(this);
+			} catch (JsonGenerationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JsonMappingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		@Override
+		public WorkUnit unpack(String packed) {
+			ObjectMapper mapper = new ObjectMapper();
+			try {
+				return mapper.readValue(packed,WorkUnit.class);
+			} catch (JsonParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JsonMappingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+		}
+		
+		public void clone(WorkUnit cloneMe) {
+			this.pathIn = new String(cloneMe.pathIn);
+			this.pathOut = new String(cloneMe.pathOut);
+			this.work = new String(cloneMe.work);
 		}
 	}
 	
--- src/main/java/com/gman/notification/KafkaProcessor.java
+++ src/main/java/com/gman/notification/KafkaProcessor.java
@@ -1,13 +1,22 @@
 package com.gman.notification;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executors;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import kafka.consumer.Consumer;
 import kafka.consumer.ConsumerConfig;
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
 import kafka.javaapi.consumer.ConsumerConnector;
+import kafka.javaapi.producer.Producer;
+import kafka.producer.ProducerConfig;
 
 public class KafkaProcessor extends BaseProcessor {
 	private static final Log LOG = LogFactory.getLog(KafkaProcessor.class);
@@ -15,20 +24,35 @@ public class KafkaProcessor extends BaseProcessor {
 	private String brokerURI;
 	private String BrokerZKURI;
 	private final ConsumerConnector consumer;
+	private final Producer<String, String> producer;
+	private ArrayBlockingQueue<WorkUnit> workUnits = new ArrayBlockingQueue<EventProcessor.WorkUnit>(10);
+	private ArrayBlockingQueue<Control> controlCommands = new ArrayBlockingQueue<EventProcessor.Control>(10);
 
 	public KafkaProcessor(String broker, String zkuri) {
 		this.brokerURI = broker;
 		this.BrokerZKURI = zkuri;
 		
-		Properties props = new Properties();
-        props.put("zookeeper.connect", zkuri);
-        //TODO: do i need different group names?
-        props.put("group.id", "group1");
-        props.put("zookeeper.session.timeout.ms", "4000");
-        props.put("zookeeper.sync.time.ms", "200");
-        props.put("auto.commit.interval.ms", "1000");
-        ConsumerConfig kconf = new ConsumerConfig(props);
+		//consumer setup
+		Properties cprops = new Properties();
+		cprops.put("zookeeper.connect", BrokerZKURI);
+        //TODO: do i need different group names? probably need to be unique
+		cprops.put("group.id", "group1");
+		cprops.put("zookeeper.session.timeout.ms", "4000");
+		cprops.put("zookeeper.sync.time.ms", "200");
+		cprops.put("auto.commit.interval.ms", "1000");
+        ConsumerConfig kconf = new ConsumerConfig(cprops);
 		consumer = Consumer.createJavaConsumerConnector(kconf);
+		LOG.info("consumer connector created");
+		
+		//producer setup
+		Properties pprops = new Properties();
+		pprops.put("metadata.broker.list", brokerURI);
+		pprops.put("serializer.class", "kafka.serializer.StringEncoder");
+		pprops.put("partitioner.class", "com.gman.broker.SimplePartitioner");
+		pprops.put("request.required.acks", "1");
+		ProducerConfig config = new ProducerConfig(pprops);
+		producer = new Producer<String, String>(config);
+		LOG.info("producer created");
 	}
 	
 	@Override
@@ -53,6 +77,63 @@ public class KafkaProcessor extends BaseProcessor {
 
 	@Override
 	public void run() {
-		
+//		ExecutorService executor;
+//	    Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+//	    topicCountMap.put(topic, new Integer(a_numThreads));
+//	    Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
+//	    List<KafkaStream<byte[], byte[]>> streams = consumerMap.get(topic);
+//	 
+//	 
+//	    // now launch all the threads
+//	    executor = Executors.newFixedThreadPool(a_numThreads);
+//	 
+//	    // now create an object to consume the messages
+//	    int threadNumber = 0;
+//	    for (final KafkaStream stream : streams) {
+//	        executor.submit(new ConsumerThread(stream, threadNumber));
+//	        threadNumber++;
+//	    }
 	}
+	
+	/**
+	 * Listens for the given object that is typed into
+	 * 
+	 * @param <T>
+	 */
+	public class ConsumerThread<T extends KMesg<T>> implements Runnable {
+	    private KafkaStream<String, String> stream;
+	    private int threadNum;
+	    private T kMesg;
+	    private ArrayBlockingQueue<T> queue;
+	 
+	    public ConsumerThread(KafkaStream stream, int threadNum, T obj,
+	    		ArrayBlockingQueue<T> queue) {
+	    	this.threadNum = threadNum;
+	    	this.stream = stream;
+	    	this.kMesg = obj;
+	    	this.queue = queue;
+	    }
+	 
+	    public void run() {
+	        ConsumerIterator<String, String> it = stream.iterator();
+	        while (it.hasNext()) {
+	        	String message = it.next().message();
+	            LOG.info("Thread " + threadNum + ": " + message);
+	            try {
+					T obj = (T) kMesg.getClass().newInstance();
+					obj.unpack(message);
+					queue.add(obj);
+				} catch (InstantiationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	            
+	        }
+	        LOG.info("Shutting down Thread: " + threadNum);
+	    }
+	}
+	
 }
